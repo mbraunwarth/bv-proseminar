3696706ed0e86df301126e379d6c2b2a44dd35bf
Markus Braunwarth <mbraunwarth@uni-koblenz.de>
2019-01-03 15:19:14 +0100

    Major changes and added geogebra file

--- ImageProcessing/src/Filter.kt
+++ ImageProcessing/src/Filter.kt
@@ -4,8 +4,33 @@ import javax.imageio.ImageIO
 
 fun main(args: Array<String>) {
     val img = ImageIO.read(File("data/iron-man.jpg"))
-    convertToGrayscale(img)
-    showImage(img)
+    val mask = arrayOf(
+            arrayOf(0,-1,0),
+            arrayOf(-1,5,-1),
+            arrayOf(0,-1,0)
+    )
+
+//    convertToGrayscale(img)
+    showImage(conv(img, mask))
+//    showImage(img)
+}
+
+fun conv(img: BufferedImage, mask: Array<Array<Int>>) : BufferedImage {
+    val m_size = mask.size/2
+    val out = BufferedImage(img.width, img.height, img.type)
+
+    for (i in m_size until img.width-m_size) {
+        for (j in m_size until img.height-m_size) {
+            var rgb = 0
+            for (m in 0 until mask.size) {
+                for (n in 0 until mask.size) {
+                    rgb += img.getRGB(i, j) * mask[m][n]
+                }
+            }
+            out.setRGB(i, j, rgb)
+        }
+    }
+    return out
 }
 
 fun gauss(img: BufferedImage) {
@@ -14,9 +39,4 @@ fun gauss(img: BufferedImage) {
             intArrayOf(2,4,2),
             intArrayOf(1,2,1)
     )
-//    for (i in 1 until img.width-1)
-//        for (j in 1 until img.height-1)
-//            for (m in -1 until 1)
-//                for (n in -1 until 1)
-
 }
